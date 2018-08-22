215b02ce8ca07d4c94c56f795cc4e914a80a7ee9
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 16:10:42 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,4 +4,8 @@ def reverse_each_word(array)
   reversed_array.join(" ")
 end
 
-reverse_each_word("hello, there everybody")
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.collect {|x| x.reverse!}
+  reversed_array.join(" ")
+end
