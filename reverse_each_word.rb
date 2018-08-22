009276315cec063616260c75ec5088da3055a979
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 16:05:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def revers_each_word(string)
+def reverse_each_word(string)
   split_string = string.split(' ')
   split_string.each do |item|
   reversed = item.reverse
@@ -6,3 +6,5 @@ end
   new_string = reversed.join(' ')
   new_string
 end
+
+reverse_each_word("hello, there everybody")
