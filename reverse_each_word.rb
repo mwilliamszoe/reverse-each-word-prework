946889aad664132a0d51abbd8101fcb6a71d0a43
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 16:09:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
-def reverse_each_word(string)
-  split_string = string.split(' ')
-  split_string.each do |item|
-  end
- new_string = item.reverse.join(' ')
-  new_string
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  reversed_array.join(" ")
 end
 
 reverse_each_word("hello, there everybody")
