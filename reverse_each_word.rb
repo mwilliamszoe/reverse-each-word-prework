8a7d880338437d32a815e7d40759f6294e4aca5e
Zoe Monet Williams <mwilliamszoe@gmail.com>
2018-08-22 16:04:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,6 @@ def revers_each_word(string)
   split_string = string.split(' ')
   split_string.each do |item|
   reversed = item.reverse
-  reversed.joing(' ')
+  new_string = reversed.join(' ')
+  new_string
+end
